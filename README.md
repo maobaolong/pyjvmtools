901fb69e1d3205474ee902863fe29b319f45e3b4
maobaolong <maobaolong@139.com>
2017-01-31 17:07:23 +0800

    add print lock info throws execute stack command.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # pyjvmtools
+
+## usage
+
+usage: pyjvmtools.py [-h] -pid PID [-lockid LOCKID] [-v]
+
+A tool to print jvm lock info.
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -pid PID        specify a jvm process id which you want to print lock info.
+  -lockid LOCKID  specify a lockid to filter information.
+  -v, --verbose   increase output verbosity
+
+## example
+```bash
+$ python pyjvmtools.py -pid 1234
+total log line count = 881 , total lockCount =  10
+        <0    > 0x00000006e02d42f0 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <1    > 0x00000006e07d6718 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <2    > 0x00000006e0025cd0 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <3    > 0x00000006e00249a0 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <4    > 0x00000006e0028fe0 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <5    > 0x00000006e0851a68 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <6    > 0x00000006e002d770 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <7    > 0x00000006d99a0770 waited num    10 , run num     0. There had unlocked lock and make deadlock.
+        <8    > 0x00000006e07b24c8 waited num     1 , run num     0. There had unlocked lock and make deadlock.
+        <9    > 0x00000006e08519c0 waited num     5 , run num     0. There had unlocked lock and make deadlock.
+
+$ python pyjvmtools.py -pid 1234  -lockid 0x00000006e08519c0
+total log line count = 881 , total lockCount =  10
+        <0    > 0x00000006e08519c0 waited num     5 , run num     0. There had unlocked lock and make deadlock.
+
+$ python pyjvmtools.py -pid 1234  -lockid 0x00000006e08519c0 -v
+total log line count = 881 , total lockCount =  10
+        <0    > 0x00000006e08519c0 waited num     5 , run num     0. There had unlocked lock and make deadlock.
+          index                     daemon                Tid    Nid           Condtion ThreadName
+        ---------------------------------------------------------------------------------------------------------
+        [    0]                     daemon 0x00007fbf5800d800 0x7f5e 0x00007fc0351bd000 ThriftClientPoolGcThreads-4
+        [    1]                     daemon 0x00007fbfac001800 0x7eed 0x00007fc0357c3000 ThriftClientPoolGcThreads-3
+        [    2]                     daemon 0x00007fbf58010000 0x7eec 0x00007fc0366d2000 ThriftClientPoolGcThreads-2
+        [    3]                     daemon 0x00007fbf74072800 0x7eea 0x00007fc0362ce000 ThriftClientPoolGcThreads-1
+        [    4]                     daemon 0x00007fbf7405d000 0x7ee9 0x00007fc0353bf000 ThriftClientPoolGcThreads-0
+
+```
--- pyjvmtools.py
+++ pyjvmtools.py
@@ -1,45 +1,134 @@
 #!/usr/bin/python
 #coding:utf-8
-
+import os
 import re
+import argparse
 class ThreadInfo:
     tName = ""
+    daemon = ""
     prio = "0"
     tid = "0x0"
     nid = "0x0"
+    cid = "0x0"
     line = ""
+    lockId = "0x0"
+
+
 class LockInfo:
     lockid = "0x0"
-    waiterThead = []
+    waiterTheadList = []
 
-def helloWorld():
+    def LockInfo(self):
+        print self
+
+verbose = True
+def getLog(pid):
+    # text_file = open("/home/mbl/a.txt", "r")
+    # lines = text_file.readlines()
+    # text_file.close()
+    command = 'stack -l '
+    r = os.popen(command)
+    lines = r.readlines()
+
+    return lines
+
+def plockInfo(pid,lockid):
     lockMap = {}
-    print "hello world!"
-    text_file = open("/home/mbl/a.txt", "r")
-    lines = text_file.readlines()
-    print lines
-    print len(lines)
-    tName = ""
+    lockHolderMap = {}
+    lines = getLog(pid)
 
+    # print "lines = " , lines
+
+    threadInfo = None
     for line in lines:
-        searchObj = re.search(r'"(.*).*".*prio=.*', line, re.M | re.I)
+        searchObj = re.search(r'"(.*)"( daemon)? prio=([0-9]+) tid=(0[xX][A-Fa-f0-9]+) nid=(0[xX][A-Fa-f0-9]+) waiting on condition \[(0[xX][A-Fa-f0-9]+)\].*', line, re.M | re.I)
         if searchObj:
             # print "searchObj.group() : ", searchObj.group()
-            tName = searchObj.group(1)
-            # print "searchObj.group(1) : ", searchObj.group(1)
+            threadInfo = ThreadInfo()
+            threadInfo.tName = searchObj.group(1)
+            threadInfo.line = line.strip()
+            threadInfo.daemon = searchObj.group(2)
+            threadInfo.prio = searchObj.group(3)
+            threadInfo.tid = searchObj.group(4)
+            threadInfo.nid = searchObj.group(5)
+            threadInfo.cid = searchObj.group(6)
         else:
             searchObj = re.search(r'- parking to wait for  <(.*)> .*', line, re.M | re.I)
             if searchObj:
-                # print "thread name : " + tName
-                # print "searchObj.group() : ", searchObj.group()
-                # print "lockId : ", searchObj.group(1)
+                lockInfo = LockInfo()
+                lockInfo.waiterTheadList = []
                 if searchObj.group(1) in lockMap:
-                    print "exist " + searchObj.group(1) +" " + tName
+                    lockInfo = lockMap[searchObj.group(1)]
                 else:
-                    lockMap[searchObj.group(1)] = tName
+                    lockMap[searchObj.group(1)] = lockInfo
+                lockInfo.lockid = searchObj.group(1)
+                threadInfo.lockId = lockInfo.lockid
+                lockInfo.waiterTheadList.append(threadInfo)
+            else:
+                searchObj = re.search(r'- <(0[xX][A-Fa-f0-9]+)> .*', line, re.M | re.I)
 
-        # print line
-    text_file.close()
-    print lockMap
+                if searchObj is None:
+                     searchObj = re.search(r'- locked <(0[xX][A-Fa-f0-9]+)> .*', line, re.M | re.I)
+                if searchObj:
+                    runCount = 0
+                    if searchObj.group(1) in lockHolderMap:
+                        runCount = lockHolderMap[searchObj.group(1)]
+                    runCount += 1
+                    lockHolderMap[searchObj.group(1)] = runCount
+
+
+
+
+    print "total log line count = %d" % len(lines) ,", total lockCount = " , len(lockMap)
+
+    # print "\t", "[%5s] %18s %7s %18s %6s %18s %s" % (
+    # "index", "", "daemon", "Tid", "Nid", "Condtion", "ThreadName")
+    # print "\t--------------------------------------------------------------------------------------------------------- "
+    if lockid in lockMap:
+        plockinfobyid(lockid, 0, lockHolderMap, lockMap[lockid])
+    else:
+        lockCount = 0
+        for k, v in lockMap.iteritems():
+            plockinfobyid(k, lockCount, lockHolderMap, v)
+            lockCount += 1
+    # print lockMap
+
+
+def plockinfobyid(k, lockCount, lockHolderMap, v):
+    runNum = 0;
+    msg = ""
+    if k in lockHolderMap:
+        runNum = lockHolderMap[k]
+    if runNum == 0:
+        msg = "There had unlocked lock and make deadlock."
+    print "\t", "<%-5d> %18s waited num %5d , run num %5d. %s" % (
+        lockCount, k, len(v.waiterTheadList), runNum, msg)
+    if verbose:
+        plockedtheadinfo(v)
+
+
+def plockedtheadinfo(v):
+    print "\t", "%7s %18s %7s %18s %6s %18s %s" % (
+        "index", "", "daemon", "Tid", "Nid", "Condtion", "ThreadName")
+    print "\t--------------------------------------------------------------------------------------------------------- "
+    for index in range(len(v.waiterTheadList)):
+        curWT = v.waiterTheadList[index]
+        print "\t", "[   %2d] %18s %7s %18s %6s %18s %s" % (
+            index, "", "" if curWT.daemon == None else curWT.daemon, curWT.tid, curWT.nid,
+            curWT.cid, curWT.tName)
+    print ""
+
+
 if __name__ == '__main__':
-    helloWorld()
+    parser = argparse.ArgumentParser(description="A tool to print jvm lock info.")
+
+    parser.add_argument("-pid",  type=int, required=True,
+                        help="specify a jvm process id which you want to print lock info. ")
+    parser.add_argument("-lockid",  type=str,
+                        help="specify a lockid to filter information. ")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    plockInfo(args.pid,args.lockid)
